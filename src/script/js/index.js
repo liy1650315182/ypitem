b3c321f08f9df9525c24e244021c4e4bca5f39d3
liangziyang <1650315182@qq.com>
2018-10-05 15:14:12 +0800

    新增登录注册以及完善首页

--- src/script/js/index.js
+++ src/script/js/index.js
@@ -1,5 +1,9 @@
 !function($){
     $('.topcontent').load('header.html');
     $('.footercontent').load('footer.html');
+    $('.aside').load('sidebar.html');
+
+    $('.login-reg-h').load('login-reg-h.html');
+    $('.login-reg-f').load('login-reg-f.html');
 }(jQuery);
 
